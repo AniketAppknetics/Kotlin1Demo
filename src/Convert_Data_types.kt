f6cac388298f452d36206bc9641c9f096dcb603e
gnr <aniket.appknetics@gmail.com>
2018-02-14 15:03:45 +0530

    Convert data type

---
+++ src/Convert_Data_types.kt
@@ -0,0 +1,11 @@
+fun main(args:Array<String>){
+
+    var age:Int=31
+    println(age.toString())
+
+    //how to check its is string
+    println(age.toString() + 10) //its concat the string
+
+    println(age.toDouble()) //its concat the string
+
+}
