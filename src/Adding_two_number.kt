674866df04c632d183bec1ce637a4cb684fec5fc
gnr <aniket.appknetics@gmail.com>
2018-02-14 14:05:04 +0530

    adding two number

---
+++ src/Adding_two_number.kt
@@ -0,0 +1,12 @@
+fun main(args:Array<String>){
+
+    var first=100;
+    var second=200;
+
+    var third:Int?
+    third=first+second
+    println(third)
+
+    println("Addition of two number is ${first + second + 300 }")
+
+}
